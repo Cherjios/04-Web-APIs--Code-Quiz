61e08d014e61bdb708b1e3ece07c68197fed2e68
cherjios <slopez0286@gmail.com>
2020-06-23 20:59:33 -0700

    Working start buttom

--- index.js
+++ index.js
@@ -1,19 +1,13 @@
+var questionEl = document.getElementById("question");
 var mainEl = document.getElementById("main");
-
 
 function setTextcodingQuizchallenge (){
     var text01 = "Start Quiz Challenge";
     var text02 = "Chose the better answer for a question, if you answer correctly, another question shows up, otherwise you will lose time, total time 60 seconds";
-    var paragraph = document.createElement("p");
-    var node = document.createTextNode(text02);
-    paragraph.appendChild(node);
 
-
-    mainEl.textContent = text01;
-    mainEl.setAttribute("class", "text-top");
-    mainEl.appendChild(paragraph);
+    questionEl.textContent = text01;
+    mainEl.textContent = text02;
 }
-
 
 
 setTextcodingQuizchallenge();
