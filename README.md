b74d5d59c1a5b2b174bfb6b82da891f37ccd8a61
cherjios <slopez0286@gmail.com>
2020-06-23 23:21:04 -0700

    adding readme

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # 04-Web-APIs--Code-Quiz
+Web application where the user can solve a code quiz. The Following quiz provides Multiple Choice Questions (MCQs) related to Javascript Framework. You will have to read all the given answers and click over the correct answer. 
+ 
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes. See deployment for notes on how to deploy the project on a live system.
+
+```
+$ git clone git@github.com:Cherjios/04-Web-APIs--Code-Quiz.git
+$ cd 04-Web-APIs--Code-Quiz
+```
+Then open in your preferred text editor:
+- [vim](https://www.vim.org/) 
+- [emmacs](https://www.gnu.org/software/emacs/)
+- [visual studio code](https://code.visualstudio.com/) 
+
+## Website view
+![Web screenshot](04-Web-APIs--Code-Quiz.png)
+
+
+## Built With
+
+* [HTML](https://developer.mozilla.org/en-US/docs/Web/HTML)
+* [CSS](https://developer.mozilla.org/en-US/docs/Web/CSS)
+* [JS](https://developer.mozilla.org/en-US/docs/Web/JavaScript)
+
+## Deployed Link
+
+* [Deploy a website to github](https://www.google.com/search?rlz=1C5CHFA_enUS876US878&sxsrf=ALeKk03L-krWYrY46bsiZVRE3DFHIEULGA%3A1591932240148&ei=UPXiXsrBCLW90PEPsvWSwAY&q=deploy+a+website+on+github&oq=deploy+a+website+on+github&gs_lcp=CgZwc3ktYWIQAzIECAAQQzIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjoECCMQJzoFCAAQkQI6BQgAELEDOgcIABCxAxBDOgIIADoHCAAQFBCHAlDcMFjZUmDGVGgAcAB4AIABZogBoAeSAQQxMC4xmAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab&ved=0ahUKEwjKqbjdqfvpAhW1HjQIHbK6BGgQ4dUDCAw&uact=5)
+
+
+## Authors
+
+* **SERGIO LOPEZ** 
+
+- [Link to Portfolio Site](#)
+- [Link to Github](https://github.com/cherjios)
+- [Link to LinkedIn](https://www.linkedin.com/in/sergio-lopez-81790579)
+
+
--- index.js
+++ index.js
@@ -9,12 +9,19 @@ var CButtonEl = document.getElementById("CButton");
 var DButtonEl = document.getElementById("DButton");
 
 
+
 //List of questions
 var question1 = {question:"Q 1 - Which of the following is correct about features of JavaScript?",
     correctAnswer:"All of the above", 
     A:"JavaScript is a lightweight, interpreted programming language.", 
     B:"JavaScript is designed for creating network-centric applications.",
     C:"JavaScript is complementary to and integrated with Java."}
+
+var question2 = {question:"Q 2 - Which of the following is the correct syntax to redirect a url using JavaScript?",
+    correctAnswer:"window.location='http://www.newlocation.com", 
+    A:"document.location='http://www.newlocation.com", 
+    B:"browser.location='http://www.newlocation.com",
+    C:"navigator.location='http://www.newlocation.com"}
 
 
 
@@ -38,37 +45,106 @@ setTextcodingQuizchallenge();
 //Function to button to start quiz
 startButtonEl.addEventListener("click", function(){
     //Start time 
-    startTimer();
-    questionEl.textContent = "";
-    mainEl.textContent = "";
-    startButtonEl.style.display = "none";
-    questionEl.textContent = question1.question;
-    AButtonEl.style.display ="inline-block";
-    AButtonEl.textContent = question1.A;
-    BButtonEl.style.display ="inline-block";
-    BButtonEl.textContent = question1.B;
-    CButtonEl.style.display ="inline-block";
-    CButtonEl.textContent = question1.C;
-    DButtonEl.style.display ="inline-block";
-    DButtonEl.textContent = question1.correctAnswer;
-
+    startTimer(timeLeft);
+    
+   
 })
 
+var timeLeft = 60;
 
-function startTimer() {
-    var timeLeft = 60;
-  
+function startTimer(timeLeft) {
+    
     var timeInterval = setInterval(function() {
       timerEl.textContent = timeLeft + " seconds remaining";
       timeLeft--;
+      MakeQuestion1();
+
   
       if (timeLeft === 0) {
         timerEl.textContent = "";
         clearInterval(timeInterval);
-        speedRead();
+        questionEl.textContent = "Game Over";
+        mainEl.textContent = "";
+        AButtonEl.style.display ="none";
+        BButtonEl.style.display ="none";
+        CButtonEl.style.display ="none";
+        DButtonEl.style.display ="none";
         
       }
   
     }, 1000);
+
+    function MakeQuestion1(){
+        questionEl.textContent = "";
+        mainEl.textContent = "";
+        startButtonEl.style.display = "none";
+        questionEl.textContent = question1.question;
+        AButtonEl.style.display ="inline-block";
+        AButtonEl.textContent = question1.A;
+        BButtonEl.style.display ="inline-block";
+        BButtonEl.textContent = question1.B;
+        CButtonEl.style.display ="inline-block";
+        CButtonEl.textContent = question1.C;
+        DButtonEl.style.display ="inline-block";
+        DButtonEl.textContent = question1.correctAnswer;
+        //Wrong Answer
+        AButtonEl.addEventListener("click",function(){
+            clearInterval(timeInterval);
+            // timeLeft = timeLeft - 15;
+            // startTimer(timeLeft);
+        })
+        //Right Answer
+        DButtonEl.addEventListener("click",function(){
+            MakeQuestion2();
+        })
+    }
+
+
   }
 
+// function MakeQuestion1(){
+//     questionEl.textContent = "";
+//     mainEl.textContent = "";
+//     startButtonEl.style.display = "none";
+//     questionEl.textContent = question1.question;
+//     AButtonEl.style.display ="inline-block";
+//     AButtonEl.textContent = question1.A;
+//     BButtonEl.style.display ="inline-block";
+//     BButtonEl.textContent = question1.B;
+//     CButtonEl.style.display ="inline-block";
+//     CButtonEl.textContent = question1.C;
+//     DButtonEl.style.display ="inline-block";
+//     DButtonEl.textContent = question1.correctAnswer;
+//     //Wrong Answer
+//     AButtonEl.addEventListener("click",function(){
+//         var timeLeft = 0;
+//         clearInterval(startTimer);
+//         timeLeft = timeLeft - 15;
+//         startTimer(timeLeft);
+//     })
+//     //Right Answer
+//     DButtonEl.addEventListener("click",function(){
+//         MakeQuestion2();
+//     })
+// }
+
+function MakeQuestion2(){
+    startButtonEl.style.display = "none";
+    questionEl.textContent = question2.question;
+    AButtonEl.style.display ="inline-block";
+    AButtonEl.textContent = question2.A;
+    BButtonEl.style.display ="inline-block";
+    BButtonEl.textContent = question2.B;
+    CButtonEl.style.display ="inline-block";
+    CButtonEl.textContent = question2.C;
+    DButtonEl.style.display ="inline-block";
+    DButtonEl.textContent = question2.correctAnswer;
+    AButtonEl.addEventListener("click",function(){
+        
+        
+    })
+    //Right Answer
+    SButtonEl.addEventListener("click",function(){
+        MakeQuestion2();
+    })
+}
