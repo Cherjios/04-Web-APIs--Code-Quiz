6bb92c9f313b36597f948f84917e04e747043655
cherjios <slopez0286@gmail.com>
2020-06-23 21:01:44 -0700

    Working start buttom

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var mainEl = document.getElementById("main");
 
 function setTextcodingQuizchallenge (){
     var text01 = "Start Quiz Challenge";
-    var text02 = "Chose the better answer for a question, if you answer correctly, another question shows up, otherwise you will lose time, total time 60 seconds";
+    var text02 = "Chose the better answer for a question, if you answer correctly, another question shows up, otherwise you will lose time, total time 60 seconds.";
 
     questionEl.textContent = text01;
     mainEl.textContent = text02;
