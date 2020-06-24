8fad69f2a6d6855dc228a5773665026d822b5e5a
cherjios <slopez0286@gmail.com>
2020-06-23 21:13:53 -0700

    Start timer function

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 var questionEl = document.getElementById("question");
 var mainEl = document.getElementById("main");
+var startButtonEl = document.getElementById("startButton");
+var timerEl = document.getElementById("timer");
 
+//Function to start text coding quiz challenge
 function setTextcodingQuizchallenge (){
     var text01 = "Start Quiz Challenge";
     var text02 = "Chose the better answer for a question, if you answer correctly, another question shows up, otherwise you will lose time, total time 60 seconds.";
@@ -9,8 +12,30 @@ function setTextcodingQuizchallenge (){
     mainEl.textContent = text02;
 }
 
-
 setTextcodingQuizchallenge();
 
+//Function to button to start quiz
+startButtonEl.addEventListener("click", function(){
+    //Start time 
+    startTimer();
+    
+})
 
+
+function startTimer() {
+    var timeLeft = 60;
+  
+    var timeInterval = setInterval(function() {
+      timerEl.textContent = timeLeft + " seconds remaining";
+      timeLeft--;
+  
+      if (timeLeft === 0) {
+        timerEl.textContent = "";
+        clearInterval(timeInterval);
+        speedRead();
+        
+      }
+  
+    }, 1000);
+  }
 
